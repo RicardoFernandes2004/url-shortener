7f01c9f353b4db6b70ab8f6eabfe78a9377a2122
RicardoFernandes2004 <Ricardo.Fernandes02082004@gmail.com>
2025-07-31 02:15:28 -0300

    feat: Cors Configured

--- src/main/java/com/alekrin/urlshortener/configurations/CorsConfig.java
+++ src/main/java/com/alekrin/urlshortener/configurations/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // COLOCAR O DOMINIO CERTO Q VAI SER QND EU FOR SUBIR O BACK
+                        .allowedOrigins("https://cademeulink.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
